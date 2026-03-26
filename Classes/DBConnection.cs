740cdad9ff2cc3c13f0be06473c8f190f6ac178a
nightSanches <galesavich@gmail.com>
2026-03-26 08:00:19 +0500

    Настроен Program.cs. Добавлен файл подключения к БД

---
+++ Classes/DBConnection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameAPI.Classes
+{
+    public class DBConnection : DbContext
+    {
+        // Пример таблицы пользователей
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseConnection()
+        {
+            Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Конфигурация подключения к базе данных MySQL
+        /// </summary>
+        /// <param name="optionsBuilder">Билдер опций контекста БД</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;port=3306;uid=root;pwd=;database=GameDatabase",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+
+        /// <summary>
+        /// Конфигурация моделей базы данных
+        /// </summary>
+        /// <param name="modelBuilder">Билдер моделей</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Пример настройки таблицы Users
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,40 @@
+using GameAPI.Classes;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Equipment Management API",
+        Version = "v1",
+        Description = "API для управления оборудованием"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+
+builder.Services.AddDbContext<DBConnection>();
+builder.Services.AddScoped<RoleChecker>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapControllers();
 
 app.Run();
